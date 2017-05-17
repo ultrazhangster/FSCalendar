90f6701a6256316dfaad5f6884e3e594a8948200
Aris Ren <marspotetial@gmail.com>
2017-05-16 22:28:08 -0700

    show today view

--- Example-Objc/DIYCalendarCell.m
+++ Example-Objc/DIYCalendarCell.m
@@ -22,7 +22,7 @@
         [self.contentView.layer insertSublayer:selectionLayer below:self.titleLabel.layer];
         self.selectionLayer = selectionLayer;
         
-        self.shapeLayer.hidden = YES;
+        self.shapeLayer.hidden = NO;
         self.backgroundView = [[UIView alloc] initWithFrame:self.bounds];
         self.backgroundView.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.1];
         
